9e86d835a28527d150f7c3964abd7ff2901895ea
duhongpeng <18638675345@163.com>
2023-04-12 21:15:38 +0800

    使用TheGraph检索event

---
+++ README.md
@@ -0,0 +1,4 @@
+##一.sungraph playgrpund
+    https://thegraph.com/hosted-service/subgraph/ddlovecc/testbids
+##二.检索截图
+![image](https://)
--- src/crowdfunding.ts
+++ src/crowdfunding.ts
@@ -1,17 +1,17 @@
-import { bids as bidsEvent } from "../generated/Crowdfunding/Crowdfunding"
-import { bids } from "../generated/schema"
+import {bids as bidsEvent} from "../generated/Crowdfunding/Crowdfunding"
+import {crowdfunding} from "../generated/schema"
 
 export function handlebids(event: bidsEvent): void {
-  let entity = new bids(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.campaignId = event.params.campaignId
-  entity.account = event.params.account
-  entity.amount = event.params.amount
+    let entity = crowdfunding.load(event.params.campaignId.toString());
+    if(entity == null){
+      entity = new crowdfunding(event.params.campaignId.toString());
+      entity.campaignId = event.params.campaignId
+      entity.account = event.params.account
+      entity.amount = event.params.amount
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+      entity.blockNumber = event.block.number
+      entity.blockTimestamp = event.block.timestamp
+      entity.transactionHash = event.transaction.hash
+      entity.save()
+    }
 }
